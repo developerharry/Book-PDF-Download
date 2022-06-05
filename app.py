82c034ee79e42462fb9ce06d13be2772012af36f
developerharry <50603405+developerharry@users.noreply.github.com>
2022-06-05 18:01:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app.secret_key ="kspilchfi"
 def index():
     return render_template("index.html")
 
-@app.route("/loading")
+@app.route("/loading", methods=["POST", "GET"])
 def loading():
     return render_template("loading.html")
 
